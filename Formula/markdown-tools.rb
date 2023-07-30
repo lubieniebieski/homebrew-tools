b170579a26406969b1f8f872fded9115ec899d35
Adam Nowak <adam.nowak@netguru.com>
2023-07-30 22:59:05 +0200

    Update markdown-tools.rb

--- Formula/markdown-tools.rb
+++ Formula/markdown-tools.rb
@@ -1,10 +1,9 @@
 class MarkdownTools < Formula
-  desc "A collection of tools for working with markdown files"
+  desc "Collection of tools for working with markdown files"
   homepage "https://github.com/lubieniebieski/markdown-tools"
   url "https://github.com/lubieniebieski/markdown-tools/archive/v0.1-alpha.tar.gz"
   sha256 "42ea39305081700da89de4ebc657a35fe91313964f888f84f7f991f4d4582ef8"
   license "MIT"
-  version "0.1-alpha"
 
   depends_on "go" => :build
 
