589f3428b45ed1b4ab55bc452c53d600dd688501
Adam Nowak <134795+lubieniebieski@users.noreply.github.com>
2023-08-07 17:35:42 +0000

    markdown-tools: update 0.2.1 bottle.

--- Formula/markdown-tools.rb
+++ Formula/markdown-tools.rb
@@ -6,9 +6,9 @@ class MarkdownTools < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/lubieniebieski/homebrew-tools/releases/download/markdown-tools-0.2"
-    sha256 cellar: :any_skip_relocation, ventura:      "1f8fe9e113f8a611f11b3963681e4b1a35fce84e393d0da8fb11bbd4a9aa08d1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "02262af334defa54dd1f8fe7dc1deddcc0c6c871b289b2056e958cbff8e3dc6b"
+    root_url "https://github.com/lubieniebieski/homebrew-tools/releases/download/markdown-tools-0.2.1"
+    sha256 cellar: :any_skip_relocation, ventura:      "892c78dcda9d2b7d74e983c8ceb00a5ea583d8ba311de325b98dee19a69b49ea"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "4bc7988a1fa85b24e0d2818287d257e65076a283512c6742f930108c2d54e6aa"
   end
 
   depends_on "go" => :build
