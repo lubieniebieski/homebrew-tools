6f3657518fcc50d3145e8bd028ef5a40287a4331
Adam Nowak <adam.nowak@netguru.com>
2023-08-07 18:30:37 +0200

    Update markdown-tools.rb
    Closes #3.
    
    Signed-off-by: Adam Nowak <134795+lubieniebieski@users.noreply.github.com>

--- Formula/markdown-tools.rb
+++ Formula/markdown-tools.rb
@@ -1,8 +1,8 @@
 class MarkdownTools < Formula
   desc "Collection of tools for working with markdown files"
   homepage "https://github.com/lubieniebieski/markdown-tools"
-  url "https://github.com/lubieniebieski/markdown-tools/archive/v0.1-alpha.tar.gz"
-  sha256 "2472293126da3768acf3016da0e1cc8c77ab2660245f25a80487438182d0b28b"
+  url "https://github.com/lubieniebieski/markdown-tools/archive/v0.2.tar.gz"
+  sha256 "532832fa748ea67740abe6e5a37926ba5b2acbf9e090be7440d7ffb120d86a63"
   license "MIT"
 
   depends_on "go" => :build
